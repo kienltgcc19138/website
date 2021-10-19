a4bae053de708f772896ad04fcc73e4e3b2b203c
Lê Trung Kiên <91647989+kienltgcc19138@users.noreply.github.com>
2021-10-19 13:59:47 +0700

    add new file

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 
 <?php
-    $conn = pg_connect("postgres://rxetzggwksapao:e3b56f892acc604496dba6d951cbb5581065c36c3c8a7e36d106fefe8467c3cd@ec2-54-145-224-156.compute-1.amazonaws.com:5432/dd3jqrmclf83qb");
+    $conn = pg_connect("postgres://aiaxqbwhsmzzri:6c5ebfadeb81133ef3a0c425c8835e11b1259a96dd2409459ff3b4ac64d5db44@ec2-54-166-37-125.compute-1.amazonaws.com:5432/d1mterg5b72ksl");
     
     if(!$conn){
         die("Can not connect database".pg_connect_error());
