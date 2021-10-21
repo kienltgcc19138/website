459ccac6529711ce1d99f665926821c56263f8e8
Lê Trung Kiên <91647989+kienltgcc19138@users.noreply.github.com>
2021-10-21 08:39:52 +0700

    new update

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 
 <?php
-    $conn = pg_connect("postgres://aiaxqbwhsmzzri:6c5ebfadeb81133ef3a0c425c8835e11b1259a96dd2409459ff3b4ac64d5db44@ec2-54-166-37-125.compute-1.amazonaws.com:5432/d1mterg5b72ksl");
-    
-    if(!$conn){
+    $connection = pg_connect("postgres://scoonfjhijybkk:6f1473299c738c95bcad4dd493f14091db9bcec0443b99879767b9b2ed08e2dd@ec2-52-0-93-3.compute-1.amazonaws.com:5432/d1t3beo5hrtvsc");
+    //$connection = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=322001");
+    if(!$connection){
         die("Can not connect database".pg_connect_error());
     } 
 ?>
